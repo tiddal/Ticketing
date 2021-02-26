2dc28f382c1169ff426415937a05e1d20363b077
tiddal <tiddal96@gmail.com>
2021-02-26 20:59:55 +0000

    ingress service setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express, { json } from 'express';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (request, response) => {
+  response.send('Hi there');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
