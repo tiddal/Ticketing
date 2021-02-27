3ab1e3791d482b33c0515ef65cdd8756dae56a0d
tiddal <tiddal96@gmail.com>
2021-02-27 10:14:35 +0000

    auth: set up validation

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,9 +1,28 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.post('/api/users/sign-up', (request, response) => {
-  const { email, password } = request.body;
-});
+router.post(
+  '/api/users/sign-up',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters long')
+  ],
+  (request: Request, response: Response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) return response.status(400).send(errors.array());
+
+    const { email, password } = request.body;
+
+    console.log('Creating an user...');
+    response.send({});
+  });
 
 export { router as signUpRouter };
