38b4dd67db03e0d68f9dbd8b96f728fd7f45a04d
tiddal <tiddal96@gmail.com>
2021-03-03 10:53:26 +0000

    infra: set up a MONGO_URI env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,8 +3,9 @@ import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+  if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
   try {
-    await connect('mongodb://auth-mongo-srv:27017/auth', {
+    await connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
