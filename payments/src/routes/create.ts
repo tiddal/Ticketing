00f8423e6112c16d68776f9cdaaad1172c865d89
tiddal <tiddal96@gmail.com>
2021-03-07 18:01:01 +0000

    payments: set up stripe

--- payments/src/routes/create.ts
+++ payments/src/routes/create.ts
@@ -2,6 +2,7 @@ import { BadRequestError, NotFoundError, OrderStatus, requireAuth, UnauthorizedE
 import { Request, Response, Router } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = Router();
 
@@ -24,6 +25,13 @@ router.post(
     if (!order) throw new NotFoundError();
     if (order.userId !== request.user!.id) throw new UnauthorizedError();
     if (order.status === OrderStatus.CANCELLED) throw new BadRequestError('Cannot pay for a cancelled order');
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token
+    });
+
     response.send({ success: true });
   }
 );
